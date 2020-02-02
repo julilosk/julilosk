8dec0bfa75ee80d5d3895e2aef9c4aaf6df52c2e
Julia <loskutovajm@mail.ru>
2020-02-02 21:20:31 +0700

    0202

--- script.js
+++ script.js
@@ -23,6 +23,39 @@
 // alert(appData.budget / 30);
 
 
-if (2*4=8) {
-    console.log("+")
+// if (2*4==8) {
+//     console.log("+")
+// } else {
+//     console.log("-")
+// }
+
+let num = 50;
+
+if (num < 49) {
+    console.log("wrong");
+} else if (num > 100) {
+    console.log("many");
+} else {
+    console.log("right");
+};
+
+(num == 50) ? console.log('right') : console.log('wrong');
+
+switch (num) {
+    case num < 49:
+        console.log('wrong');
+        break;
+    case num > 100:
+        console.log('many');
+        break;
+    case naum > 80:
+        console.log('many too');
+        break;
+    case 50: /* без знака равно при switch */
+        console.log('right');
+        break;
+    default:
+        console.log('что-то пошло не так');
+        break;
+
 }
